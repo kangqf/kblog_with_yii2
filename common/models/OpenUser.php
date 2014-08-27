cea6da043f5f4eec9cce09b34f262a400677fedc
kangqf <kangqingfei@163.com>
2014-08-27 23:37:56 +0800

    使用phpstrom格式化怠慢

--- common/models/OpenUser.php
+++ common/models/OpenUser.php
@@ -2,7 +2,6 @@
 
 namespace common\models;
 
-
 //第三方用户信息提取类
 class OpenUser
 {
@@ -11,80 +10,76 @@ class OpenUser
     public $name;
     public $email;
 
-
     function __construct($client)
     {
-        switch($client->getId())
-        {
-          case 'tencent':
-            $attributes = $client->getUserAttributes();
-              $this->openId = $client->openid;
-              $this->avatarUrl = $attributes['figureurl_2'];
-            $this->name = $attributes['nickname'];
-            $this->email = '';
-              // $tmp = file_get_contents($this->avatarUrl);
-              // dump($tmp);
-              break;
+        switch ($client->getId()) {
+            case 'tencent':
+                $attributes = $client->getUserAttributes();
+                $this->openId = $client->openid;
+                $this->avatarUrl = $attributes['figureurl_2'];
+                $this->name = $attributes['nickname'];
+                $this->email = '';
+                // $tmp = file_get_contents($this->avatarUrl);
+                // dump($tmp);
+                break;
 
-          case 'weibo':
-            $attributes = $client->getUserAttributes();
-              $this->openId = $attributes['id'];
-              $this->avatarUrl = $attributes['avatar_hd'];
-            $this->name = $attributes['name'];
-            $this->email = '';
-            break;
+            case 'weibo':
+                $attributes = $client->getUserAttributes();
+                $this->openId = $attributes['id'];
+                $this->avatarUrl = $attributes['avatar_hd'];
+                $this->name = $attributes['name'];
+                $this->email = '';
+                break;
 
-          case 'github':
-            $attributes = $client->getUserAttributes();
-              $this->openId = $attributes['id'];
-              $this->avatarUrl = $attributes['avatar_url'];
-            $this->name = $attributes['name'];
-            $this->email = $attributes['email'];
-            break;
+            case 'github':
+                $attributes = $client->getUserAttributes();
+                $this->openId = $attributes['id'];
+                $this->avatarUrl = $attributes['avatar_url'];
+                $this->name = $attributes['name'];
+                $this->email = $attributes['email'];
+                break;
 
-          default:
-            $this->openId = '';
-            $this->avatarUrl = '';
-            $this->name = '';
-            $this->email = '';
-            break;
+            default:
+                $this->openId = '';
+                $this->avatarUrl = '';
+                $this->name = '';
+                $this->email = '';
+                break;
 
         }
 
-
-    }
-    public function getClientAttributes(){
-
-
-
     }
 
-    public static function grabImage($url,$filename="", $path = "@webroot/avatar/" )
+    public function getClientAttributes()
     {
-      	if ($url == "") return false;
 
-        $ext=strrchr($url,"."); //获取扩展名
-        $ext_arr = array(".gif",".png",".jpg",".bmp");
+
+    }
+
+    public static function grabImage($url, $filename = "", $path = "@webroot/avatar/")
+    {
+        if ($url == "") return false;
+
+        $ext = strrchr($url, "."); //获取扩展名
+        $ext_arr = array(".gif", ".png", ".jpg", ".bmp");
 
         //判断扩展名是否为图片
         if (!in_array($ext, $ext_arr)) return false;
 
-        if($filename == "")
-        {
-      		//我就随便将图片文件名保存为时间戳了，你可自行修改
-      		$filename = time().$ext;
-      	}
-      	ob_start(); //打开浏览器的缓冲区
-      	readfile($url); //将图片读入缓冲区
-      	$img = ob_get_contents(); //获取缓冲区的内容复制给变量$img
-      	ob_end_clean(); //关闭并清空缓冲
-      	$fp = @fopen($filename,"a"); //将文件绑定到流
-      	fwrite($fp,$img); //写入文件
-      	fclose($fp); //关闭文件之争
-      	return $filename;
+        if ($filename == "") {
+            //我就随便将图片文件名保存为时间戳了，你可自行修改
+            $filename = time() . $ext;
+        }
+        ob_start(); //打开浏览器的缓冲区
+        readfile($url); //将图片读入缓冲区
+        $img = ob_get_contents(); //获取缓冲区的内容复制给变量$img
+        ob_end_clean(); //关闭并清空缓冲
+        $fp = @fopen($filename, "a"); //将文件绑定到流
+        fwrite($fp, $img); //写入文件
+        fclose($fp); //关闭文件之争
+        return $filename;
     }
 
-
-
-
 }
+
+?>
