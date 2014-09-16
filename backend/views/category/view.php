6c1b577ea6b61e78933ed0871b3374e239ad05e5
kangqf <kangqingfei@163.com>
2014-09-16 23:28:06 +0800

    完善DetailView`

--- backend/views/category/view.php
+++ backend/views/category/view.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use kartik\detail\DetailView;
 use kartik\datecontrol\DateControl;
+use common\models\Category;
 
 /**
  * @var yii\web\View $this
@@ -13,9 +14,9 @@ $this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="category-view">
+<div class="category-view col-lg-6 col-lg-offset-3">
     <div class="page-header">
-        <h1><?= Html::encode($this->title) ?></h1>
+        <h3><?= Html::encode($this->title) ?></h3>
     </div>
 
 
@@ -32,12 +33,12 @@ $this->params['breadcrumbs'][] = $this->title;
 
         'attributes' => [
          //   'cgid',
-            ['attribute'=>'level', 'type'=>DetailView::INPUT_DROPDOWN_LIST],
+            ['attribute'=>'level','items' => Category::getLevelArray(), 'type'=>DetailView::INPUT_DROPDOWN_LIST],
            // 'level',
             'name',
-            'status',
-            'visual_able',
-            'parent_id',
+         //   'status',
+            ['attribute'=>'visual_able', 'type'=>DetailView::INPUT_DROPDOWN_LIST,'items' => Category::getVisualEnableArray(),],
+            ['attribute'=>'parent_id', 'type'=>DetailView::INPUT_DROPDOWN_LIST,'items' => Category::getTopCategoryArray(),],
         ],
 
         'deleteOptions'=>[
