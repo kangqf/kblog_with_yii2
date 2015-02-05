e23153f81bc01b771fb9444609629fb95f9aa6a4
kangqf <kangqingfei@163.com>
2015-02-05 14:47:09 +0800

    delete chinese in console as it doesn't support

--- console/controllers/HelloController.php
+++ console/controllers/HelloController.php
@@ -22,7 +22,7 @@ class HelloController extends Controller
      * This command echoes what you have entered as the message.
      * @param string $message the message to be echoed.
      */
-    public function actionIndex($message = '你好世界 hello world')
+    public function actionIndex($message = 'hello world')
     {
         echo $message . "\n";
     }
